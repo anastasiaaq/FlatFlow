7c77e9c7bf818a9a94b34189e4e3cf3a0c326955
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2026-06-04 07:26:19 +0300

    fix(#16): remove unused app

--- src/backend/flatflow/config/settings.py
+++ src/backend/flatflow/config/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
-    'django.contrib.sites',
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'drf_spectacular',
